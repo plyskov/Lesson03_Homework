e889ff59933d0eee12b30122cd2f5bf72cefff3b
Pavel <lp.qroovemaster@gmail.com>
2023-01-02 00:18:28 +0300

    Lesson 03 homework, task 17: done

--- src/Task_17/Main.java
+++ src/Task_17/Main.java
@@ -7,22 +7,22 @@ class BankAccount {
     int moneyAmount;
 
     BankAccount(String ownerFirstName,
-                String ownerLastName
+                String ownerLastName,
                 int moneyAmount) {
         this.ownerFirstName = ownerFirstName;
         this.ownerLastName = ownerLastName;
-        this.money = money;
+        this.moneyAmount = moneyAmount;
     }
 
-    String getOwnerFirstName( {
-        return this.ownerFirstName
+    String getOwnerFirstName() {
+        return this.ownerFirstName;
     }
 
-    String getOwnerLastName) {
-        return this.ownerFirstName
+    String getOwnerLastName() {
+        return this.ownerLastName;
     }
 
-    void getMoneyAmount() {
+    int getMoneyAmount() {
         return this.moneyAmount;
     }
 
@@ -33,8 +33,8 @@ class BankAccountDemo {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount("Viktor", "Savonin", 100000);
         String ownerFirstName = bankAccount.getOwnerFirstName();
-        String ownerFirstName = bankAccount.getOwnerLastName();
-        String moneyAmount = bankAccount.getMoneyAmount();
+        String ownerLastName = bankAccount.getOwnerLastName();
+        int moneyAmount = bankAccount.getMoneyAmount();
         System.out.println("Owner first name = " + ownerFirstName);
         System.out.println("Owner last name = " + ownerLastName);
         System.out.println("Money amount = " + moneyAmount);
