e44f4a176f80d563b0bd48cd4110eece293f4f08
Pavel <lp.qroovemaster@gmail.com>
2023-01-01 22:14:19 +0300

    Lesson 03 homework - task 1: class Robot created

---
+++ src/Task01_Robot/Robot.java
@@ -0,0 +1,9 @@
+package Task01_Robot;
+
+public class Robot {
+
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+
+}
---
+++ src/Task01_Robot/RobotDemo.java
@@ -0,0 +1,10 @@
+package Task01_Robot;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+        Robot myRobo = new Robot();
+        myRobo.sayHello();
+    }
+
+}
