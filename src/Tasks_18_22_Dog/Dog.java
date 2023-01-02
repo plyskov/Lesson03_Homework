414e0d5119c3222e40e70c843abcfd4173436958
Pavel <lp.qroovemaster@gmail.com>
2023-01-02 20:59:57 +0300

    Lesson 03 homework, task 21: done

--- src/Tasks_18_22_Dog/Dog.java
+++ src/Tasks_18_22_Dog/Dog.java
@@ -4,14 +4,16 @@ public class Dog {
 
     private final String name;
     private int age;
+    private String color;
 
-    Dog(String name, int age){
-        this.name = name;
-        this.age  = age;
+    Dog(String name, int age, String color){
+        this.name  = name;
+        this.age   = age;
+        this.color = color;
     }
 
     public void voice() {
-        System.out.println("Name: " + name + "; age: " + age + ".");
+        System.out.println("Name: " + name + "; age: " + age + "; color: " + color + ".");
     }
 
     public void happyBirthday(){
--- src/Tasks_18_22_Dog/DogDemo.java
+++ src/Tasks_18_22_Dog/DogDemo.java
@@ -2,7 +2,7 @@ package Tasks_18_22_Dog;
 
 public class DogDemo {
     public static void main(String[] args) {
-        Dog myDog = new Dog("Archi", 5);
+        Dog myDog = new Dog("Archi", 5, "Brown");
         myDog.voice();
         myDog.happyBirthday();
         myDog.voice();
